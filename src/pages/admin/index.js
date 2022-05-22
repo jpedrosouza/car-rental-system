c519b74a58e82a719809cdd6f114a2391f9119a5
João Pedro Souza <jsouza.1650@gmail.com>
2022-05-22 19:35:20 -0300

    remove 'Alugueis' option and set justify-center in options on admin index page

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -14,7 +14,7 @@ export default function Admin() {
       <main className="w-screen h-full overflow-hidden flex flex-col justify-center items-center">
         <h1>Área Administrativa Aluga Mais</h1>
 
-        <div className="w-1/2 flex flex-row mt-20">
+        <div className="w-1/2 flex flex-row mt-20 justify-center">
           <div onClick={() => navigateToPage("/admin/cars")} className="w-60 h-60 p-5 flex flex-col mr-5 shadow-2xl rounded-xl justify-center items-center cursor-pointer">
             <h1>Carros</h1>
             <span className="mt-5 text-center font-thin">Liste, adicione e atualize registros de veículos</span>
@@ -22,10 +22,6 @@ export default function Admin() {
           <div onClick={() => navigateToPage("/admin/customers")} className="w-60 h-60 p-5 flex flex-col mr-5 shadow-2xl rounded-xl justify-center items-center cursor-pointer">
             <h1>Clientes</h1>
             <span className="mt-5 text-center font-thin">Liste, adicione e atualize registros de clientes</span>
-          </div>
-          <div onClick={() => navigateToPage("/admin/rents")} className="w-60 h-60 p-5 flex flex-col mr-5 shadow-2xl rounded-xl justify-center items-center cursor-pointer">
-            <h1>Alugueis</h1>
-            <span className="mt-5 text-center font-thin">Liste, adicione e atualize registros de alugueis</span>
           </div>
         </div>
       </main>
