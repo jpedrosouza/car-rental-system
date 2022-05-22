e82fd07f3e3cdae28053d2a10282a90f9d62ffb0
João Pedro Souza <jsouza.1650@gmail.com>
2022-05-22 15:01:43 -0300

    added navigate method to navigate to pages when clicked on options on index page

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,6 +1,10 @@
 import Head from "next/head";
 
 export default function Admin() {
+  const navigateToPage = (page) => {
+    window.location.href = page;
+  }
+
   return (
     <div className="w-screen h-screen">
       <Head>
@@ -11,15 +15,15 @@ export default function Admin() {
         <h1>Área Administrativa Aluga Mais</h1>
 
         <div className="w-1/2 flex flex-row mt-20">
-          <div className="w-60 h-60 p-5 flex flex-col mr-5 shadow-2xl rounded-xl justify-center items-center cursor-pointer">
+          <div onClick={() => navigateToPage("/admin/cars")} className="w-60 h-60 p-5 flex flex-col mr-5 shadow-2xl rounded-xl justify-center items-center cursor-pointer">
             <h1>Carros</h1>
             <span className="mt-5 text-center font-thin">Liste, adicione e atualize registros de veículos</span>
           </div>
-          <div className="w-60 h-60 p-5 flex flex-col mr-5 shadow-2xl rounded-xl justify-center items-center cursor-pointer">
+          <div onClick={() => navigateToPage("/admin/customers")} className="w-60 h-60 p-5 flex flex-col mr-5 shadow-2xl rounded-xl justify-center items-center cursor-pointer">
             <h1>Clientes</h1>
             <span className="mt-5 text-center font-thin">Liste, adicione e atualize registros de clientes</span>
           </div>
-          <div className="w-60 h-60 p-5 flex flex-col mr-5 shadow-2xl rounded-xl justify-center items-center cursor-pointer">
+          <div onClick={() => navigateToPage("/admin/rents")} className="w-60 h-60 p-5 flex flex-col mr-5 shadow-2xl rounded-xl justify-center items-center cursor-pointer">
             <h1>Alugueis</h1>
             <span className="mt-5 text-center font-thin">Liste, adicione e atualize registros de alugueis</span>
           </div>
